7fe98a083a730c76171ac0911607a5735cc2841f
ihesro <2770520782@qq.com>
2020-07-24 22:28:13 +0800

    1.0.0

---
+++ static/admin/header.php
@@ -0,0 +1,54 @@
+<?php
+if (!defined('__TYPECHO_ADMIN__')) {
+    exit;
+}
+
+$header = '<link rel="stylesheet" href="' . Typecho_Common::url('normalize.css?v=' . $suffixVersion, $options->adminStaticUrl('css')) . '">
+<link rel="stylesheet" href="' . Typecho_Common::url('grid.css?v=' . $suffixVersion, $options->adminStaticUrl('css')) . '">
+<link rel="stylesheet" href="' . Typecho_Common::url('style.css?v=' . $suffixVersion, $options->adminStaticUrl('css')) . '">
+<!--[if lt IE 9]>
+<script src="' . Typecho_Common::url('html5shiv.js?v=' . $suffixVersion, $options->adminStaticUrl('js')) . '"></script>
+<script src="' . Typecho_Common::url('respond.js?v=' . $suffixVersion, $options->adminStaticUrl('js')) . '"></script>
+<![endif]-->';
+
+/** 注册一个初始化插件 */
+$header = Typecho_Plugin::factory('admin/header.php')->header($header);
+?><!DOCTYPE HTML>
+<html class="no-js">
+    <head>
+        <meta charset="<?php $options->charset(); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="renderer" content="webkit">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php _e('%s - %s - Powered by Typecho', $menu->title, $options->title); ?></title>
+        <meta name="robots" content="noindex, nofollow">
+		<?php echo $header; ?>
+		<link rel="stylesheet" href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css">
+		<link rel="stylesheet" href="<?php $options->siteUrl(); ?>/user/user.css?v=1.02">
+	    	<link rel="stylesheet" href="<?php $options->siteUrl(); ?>/user/user-style.css?v=1.02">
+		<script>
+		var UserLink="<?php $options->adminUrl('profile.php'); ?>";
+		var UserPic="<?php $email =$user->mail; if($email){if(strpos($email,'@qq.com') !==false){$email=str_replace('@qq.com','',$email);echo '//q1.qlogo.cn/g?b=qq&nk='.$email.'&';}else{$email= md5($email);echo '//secure.gravatar.com/avatar/'.$email.'?';}}else{echo '//secure.gravatar.com/avatar/null?';} ?>";
+		var AdminLink="<?php $options->adminUrl(); ?>";
+		var SiteLink="<?php $options->siteUrl(); ?>";
+		var UserName="<?php $user->screenName(); ?>";
+		var UserGroup="<?php $user->group(); ?>";
+		var SiteName="<?php $options->title(); ?>";
+		var MenuTitle="<?php $menu->title(); ?>";
+		</script>
+		<style>
+		<?php if(($menu->title == "网站概要")||($menu->title == "")): ?>
+		.typecho-page-main div:nth-child(4){display:none;}
+		.typecho-page-main .col-tb-4{width:50%;}
+		<?php endif; ?>
+		<?php if($menu->title == "登录到".$options->title): ?>
+		.popup{width:100% !important;
+		left:0px !important;
+		top:0px !important;}
+		<?php endif; ?>
+		</style>
+    </head>
+    <body<?php if (isset($bodyClass)) {echo ' class="' . $bodyClass . '"';} ?>>
+    <!--[if lt IE 9]>
+        <div class="message error browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
+    <![endif]-->
---
+++ static/user/user.js
@@ -0,0 +1,93 @@
+﻿// typecho user V1.11
+// 不暇 QQ1378283361
+// www.ruletree.club
+function getElementsClass(classnames){ 
+	var classobj= new Array();
+	var classint=0;
+	var tags=document.getElementsByTagName("*"); 
+	for(var i in tags){
+		if(tags[i].nodeType==1){
+		if(tags[i].getAttribute("class") == classnames){
+			classobj[classint]=tags[i];
+			classint++;
+			} 
+		}
+	}
+	return classobj;
+} 
+$(function(){
+	var UserInfo = "<div class=\"user-info\"><a href=\""+AdminLink+"\"><img src=\""+UserPic+"s=100\" /></a><p>欢迎您，<a href=\""+AdminLink+"\">"+UserName+"</a></p></div>";
+  	if(UserGroup == "administrator"){
+		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+AdminLink+"index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-paper-plane\"></i>全局模块<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"profile.php\">个人设置</a></li><li><a href=\""+AdminLink+"plugins.php\">插件控制</a></li><li><a href=\""+AdminLink+"themes.php\">模板外观</a></li><li><a href=\""+AdminLink+"backup.php\">数据备份</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-pencil\"></i>快捷操作<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"write-post.php\">撰写文章</a></li><li><a href=\""+AdminLink+"write-page.php\">创建页面</a></li><li><a href=\""+AdminLink+"user.php\">新增用户</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-cube\"></i>内容管理<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"manage-posts.php\">内容文章</a></li><li><a href=\""+AdminLink+"manage-pages.php\">独立页面</a></li><li><a href=\""+AdminLink+"manage-comments.php\">用户评论</a></li><li><a href=\""+AdminLink+"manage-categories.php\">分类管理</a></li><li><a href=\""+AdminLink+"manage-tags.php\">标签管理</a></li><li><a href=\""+AdminLink+"manage-medias.php\">文件管理</a></li><li><a href=\""+AdminLink+"manage-users.php\">用户管理</a></li><li><a href=\""+AdminLink+"extending.php?panel=Handsome%2Fmanage-links.php\">友链管理</a></li></ul></li><li class=\"menu-li\"><a href=\"javascript:;\"><i class=\"fa fa-gear\"></i>网站设置<i class=\"fa fa-angle-down right\"></i></a><ul class=\"menu-ul\"><li><a href=\""+AdminLink+"options-general.php\">基本设置</a></li><li><a href=\""+AdminLink+"options-discussion.php\">评论设置</a></li><li><a href=\""+AdminLink+"options-reading.php\">阅读设置</a></li><li><a href=\""+AdminLink+"options-permalink.php\">永久链接</a></li></ul><li></ul></div>";
+	}else if(UserGroup == "editor"||UserGroup == "contributor"){
+		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+AdminLink+"index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li><a href=\""+AdminLink+"profile.php\"><i class=\"fa fa-gear\"></i>个人设置</a></li><li><a href=\""+AdminLink+"write-post.php\"><i class=\"fa fa-pencil\"></i>创建文章</a></li><li><a href=\""+AdminLink+"manage-posts.php\"><i class=\"fa fa-cube\"></i>管理文章</a></li><li><a href=\""+AdminLink+"/manage-comments.php\"><i class=\"fa fa-comments-o\"></i>管理评论</a></li></ul></div>";
+    }else{
+		var HtmlText="<div class=\"user-nav\"><ul><li><a href=\""+AdminLink+"index.php\"><i class=\"fa fa-dashboard\"></i>控制台</a></li><li><a href=\""+AdminLink+"profile.php\"><i class=\"fa fa-gear\"></i>个人设置</a></li></ul></div>";
+    }
+	var NavHtml = UserInfo+HtmlText;
+	var Nav = document.getElementById('typecho-nav-list');
+	if(UserGroup != ""){
+		$('#typecho-nav-list').html(NavHtml);
+      	var ToMain=getElementsClass("operate")[0];
+        var Main=getElementsClass("main")[0];
+        var ToNav=document.createElement('a');
+        var width=document.body.clientWidth;
+        ToNav.id="tonav";
+        ToNav.href='javascript:;';
+        ToNav.innerHTML='<i class="fa fa-bars"></i>'; 
+        ToMain.appendChild(ToNav);
+        var ToggleNav=document.createElement('tonav');
+        tonav.onclick=function(){
+            if(width>1000){
+                if(Nav.style.display == "block"){
+                    Nav.style.display="none";
+                    ToMain.style.width="calc(100% - 15px)";
+                    Main.style.width="100%";
+                } else if(Nav.style.display == "none") {
+                    Nav.style.display="block";
+                    ToMain.style.width="calc(100% - 265px)";
+                    Main.style.width="calc(100% - 270px)";
+                } else{
+                    Nav.style.display="none";
+                    ToMain.style.width="calc(100% - 15px)";
+                    Main.style.width="100%";
+                }
+            }else{
+                if(Nav.style.display == "block"){
+                    Nav.style.display="none";
+                    ToMain.style.width="100%";
+                    Main.style.width="100%";
+                } else if(Nav.style.display == "none") {
+                    Nav.style.display="block";
+                    ToMain.style.width="100%";
+                    Main.style.width="100%";
+                } else{
+                    Nav.style.display="block";
+                    ToMain.style.width="100%";
+                    Main.style.width="100%";
+                }
+            }
+        }
+	if (MenuTitle == "个人设置"){
+		var avatar=getElementsClass("profile-avatar")[0];
+		avatar.setAttribute("src",UserPic+"s=640");
+		avatar.style.width="220px";
+	}
+    }else{
+        var LoginMain=getElementsClass("i-logo")[0];
+      	var Body=getElementsClass("body-100")[0];
+      	var Cover=document.createElement('div');
+      	Cover.id="cover";
+        Body.appendChild(Cover);
+        LoginMain.innerHTML="<img src=\""+SiteLink+"user/logo.png\" alt=\""+SiteName+"\"/>";
+    }
+	$("body").on("click",".menu-li",function () {
+		$(this).find(".menu-ul").slideToggle(200);
+	});
+	$("body").on("click","#wmd-fullscreen-button",function () {
+		$(".main").addClass("main-in");
+	});
+	$("body").on("click","#wmd-exit-fullscreen-button",function () {
+		$(".main").removeClass("main-in");
+	});
+});
